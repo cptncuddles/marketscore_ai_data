41a4003e501f84345c7fd6556b05f80c98953513
cptncuddles <159074823+cptncuddles@users.noreply.github.com>
2024-07-04 09:51:48 -0500

    Add files via upload
    2 of 3 ETL Pipelines for Marketscore AI project.

---
+++ Ticker Converter.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import json
+import glob
+
+#helper function to pull name from imported file to be used in naming new file
+def pull_name(string):
+    name_list = string.split('\\')
+    for x in name_list:
+        if ".json" in x:
+            name = x.split('.')
+            return name[0]
+        else:
+            pass
+#using glob module to allow us to grab multiple files at once. This will change once we switch to an API extraction process
+json_files = glob.glob("C:\\Users\\17197\\Documents\\Coding Projects\\Market Score Data\\Individual Stock Ticker\\*.json")
+#main function for cleaning this specific dataset, all 3 programs will have a similar stucture, with small changes to accommodate each dataset
+def json_to_csv_converter(files):
+    for file in files:
+        with open(file) as json_file:
+            data = json.load(json_file)
+            df_list = []
+            data_dump = data['trend']
+#This double naming is due to meta data stored in the 0 index of the values for the .json's dict object
+            data_dump = data_dump[1:]
+            df1 = pd.DataFrame(data_dump, columns=['time_stamp', 'calls', 'puts'])
+            data_dump2 = data['candles']
+            data_dump2 = data_dump2[1:]
+            df2 = pd.DataFrame(data_dump2, columns = ['time_stamp', 'low', 'open','close', 'high', 'style', 'c_bid', 'c_ask', 'c_high', 'c_low', 'p_bid', 'p_ask', 'p_high', 'p_low'])
+            df2.drop(columns='style')
+            df3 = pd.merge(df1, df2)
+            df_list.append(df3)
+            names = [pull_name(file)]
+#Need to use zip for this double iterator so that each name get's paired properly with it's corresponding file.
+            for df, name in zip(df_list, names):
+                df.to_csv(f"C:\\Users\\17197\\Documents\\Coding Projects\\Market Score Data\\Individual Stock Ticker\\{name}.csv", index=False)
+#current file is csv format with zero index, but can be switched to parquet or even back to json. file type can be determined once a ML structure is decided upon
+
+json_to_csv_converter(json_files)
