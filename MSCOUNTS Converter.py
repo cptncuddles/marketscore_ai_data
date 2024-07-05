8ab5c47b73ebcb1338c84ddfa7832b783a1a9780
cptncuddles <159074823+cptncuddles@users.noreply.github.com>
2024-07-05 13:03:32 -0500

    Update MSCOUNTS Converter.py

--- MSCOUNTS Converter.py
+++ MSCOUNTS Converter.py
@@ -14,7 +14,7 @@ def pull_name(string):
         else:
             pass
 
-json_files = glob.glob("C:\\Users\\17197\\Documents\\Coding Projects\\Market Score Data\\MSCounts Ticker\\*json")
+json_files = glob.glob("//file_path.json")
 #main function for cleaning this specific dataset, all 3 programs will have a similar stucture, with small changes to accommodate each dataset
 def json_to_csv_converter(files):
     for file in files:
@@ -25,12 +25,11 @@ def json_to_csv_converter(files):
             data_dump = data_dump[1:]
             df1 = pd.DataFrame(data_dump, columns=['time_stamp', 'calls', 'puts'])
             display(df1)
-            '''
             df_list.append(df1)
             names = [pull_name(file)]
 #Need to use zip for this double iterator so that each name get's paired properly with it's corresponding file.   
             for df, name in zip(df_list, names):
-                df.to_csv(f"C:\\Users\\17197\\Documents\\Coding Projects\\Market Score Data\\MSCounts Ticker\\{name}.csv", index=False)
+                df.to_csv(f"//file_path{name}.csv", index=False)
 #current file is csv format with zero index, but can be switched to parquet or even back to json. file type can be determined once a ML structure is decided upon            
-            '''
+
 json_to_csv_converter(json_files)
